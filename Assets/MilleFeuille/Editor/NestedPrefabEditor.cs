bc2a4cad086753de018c97e4da0929d8d7f39e0d
kyubuns <jp@ex.cx>
2014-05-04 00:15:16 +0900

    エディタ上でNestedPrefabのScriptが描画されないようにしてみた

---
+++ Assets/MilleFeuille/Editor/NestedPrefabEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(NestedPrefab))]
+public class NestedPrefabEditor : Editor {
+	public override void OnInspectorGUI() {
+		NestedPrefab obj = target as NestedPrefab;
+		obj.prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", obj.prefab, typeof(GameObject), false);
+		if(GUI.changed) EditorUtility.SetDirty(target);
+	}
+}
--- Assets/MilleFeuille/Scripts/NestedPrefab.cs
+++ Assets/MilleFeuille/Scripts/NestedPrefab.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class NestedPrefab : MonoBehaviour {
-	[SerializeField] GameObject prefab;
+	public GameObject prefab;
 
 	void Start()
 	{
