26b72a011914fc0d12fe3720b7619661a556866b
kyubuns <jp@ex.cx>
2014-05-11 00:59:35 +0900

    Exampleシーン追加

---
+++ Assets/Example_PrefabInPrefab/Scripts/TextReceiver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextReceiver : MonoBehaviour {
+	[SerializeField] TextMesh textMesh;
+
+	public string text
+	{
+		get
+		{
+			return textMesh.text;
+		}
+		set
+		{
+			textMesh.text = value;
+		}
+	}
+}
---
+++ Assets/Example_PrefabInPrefab/Scripts/TextSender.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TextSender : MonoBehaviour {
+	[SerializeField] string text;
+
+	void Start()
+	{
+		var receiver = GetComponent<TextReceiver>();
+		if(receiver != null) receiver.text = text;
+	}
+}
